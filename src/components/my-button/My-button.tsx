7d3b069359df0b3f80b08697ce1cc447db1485b2
layla <laylahasan2001@gmail.com>
2026-04-24 12:04:24 +0300

    Button component added

---
+++ src/components/my-button/My-button.tsx
@@ -0,0 +1,8 @@
+import "./my-button.scss"
+const MyButton = () => {
+  return <button className= "my-button">My Button</button>;
+};
+export default MyButton;//to use this component, you can import it in another file like this:
+// import MyButton from './components/my-button/My-button';
+// Then you can use it in your JSX like this:
+// <MyButton />
